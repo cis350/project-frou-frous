e7cd46e518b0b9eb2649a71a0cd1ea62068acaa3
marcel kida <47465692+marcelkida@users.noreply.github.com>
2023-03-17 15:58:49 -0400

    routing

---
+++ src/Views/ScheduleView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ScheduleView() {
+  return (
+    <div>ScheduleView</div>
+  )
+}
+
+export default ScheduleView
---
+++ src/Views/UserPageView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserPageView() {
+  return (
+    <div>UserPageView</div>
+  )
+}
+
+export default UserPageView
