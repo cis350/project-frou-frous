fbbf4759c64f4282d01a229a9a9d8dd20c15f7d8
jesssling <56201226+jesssling@users.noreply.github.com>
2023-03-22 16:36:37 -0400

    user profile API

---
+++ src/api/userProfileAPI.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter'; // eslint-disable-line
+
+// This sets the mock adapter on the default instance
+const mock = new MockAdapter(axios);
+
+mock.onGet('/user/userId').reply(200, {
+    username: 'MockUsername',
+    pfp: '../assets/ria_laugh.png',
+    skipHistory: [
+        { 
+            timestamp: 1679326514, // see convertTimestamp in: ChatPeopleComponent.jsx
+            class: 'ABC 101'
+        },
+        { 
+            timestamp: 1679326514,
+            class: 'XYZ 101'
+        },
+        { 
+            timestamp: 1679326514,
+            class: 'XYZ 101'
+        },
+        { 
+            timestamp: 1679326514,
+            class: 'ABC 101'
+        },
+        { 
+            timestamp: 1679326514,
+            class: 'XYZ 101'
+        }
+    ],
+    classes: [
+        {
+            title: 'ABC 101',
+            days: ['Wed', 'Fri'],
+            start: 1300,
+            end: 1430
+        },
+        {
+            title: 'XYZ 101',
+            days: ['Wed'],
+            start: 1500,
+            end: 1700
+        },
+        {
+            title: 'ABC 201',
+            days: ['Tue', 'Thu'],
+            start: 1300,
+            end: 1430
+        }
+    ]
+})
+
+export async function getUserHistory(userId) {
+    console.log(username);
+    const res = await axios.get('/user/userId/${username}');
+    return res.data;
+}
