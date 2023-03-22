a92698d53980282a5874383924ae1611fd2b51e9
marcel kida <47465692+marcelkida@users.noreply.github.com>
2023-03-21 21:23:55 -0400

    Revert "Revert "eslint""
    This reverts commit d1193fef5a9d65b2c60f058ea202a67a7c3cd19a.

--- src/Components/AddClass.jsx
+++ src/Components/AddClass.jsx
@@ -1,86 +1,108 @@
-import React, { useRef, useState } from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 
-function AddClass(props) {
-    const baseURL = "http://localhost:3000"
-    const [title, setTitle] = useState("")
-    const [location, setLocation] = useState("")
-    const [start, setStart] = useState("")
-    const [end, setEnd] = useState("")
-    const [days, setDays] = useState(new Array(5).fill(false))
-    const submitClass = (e) => {
-        console.log(start)
-        const addNewClass = async () => {
-            for (var i =0; i <= 5; i++){
-                if (days[i]){
-                    try{
-                        const prev = await axios.get(`${baseURL}/classes/${i}`)
-                        var prevObj = prev.data
-                        prevObj.classes.push({
-                            name: title,
-                            location : location,
-                            start: start,
-                            end: end
-                        })
-                        await axios.put(`${baseURL}/classes/${i}`, prevObj)
-                    } catch (error) {
-                        console.log(error)
-                    }
-                }
-                
-            }
-            
-            
+function AddClass({ handleChild }) {
+  function defaultFunc() {
+    const please = 1;
+    return please;
+  }
+  AddClass.defaultProps = {
+    handleChild: defaultFunc(),
+  };
+  AddClass.propTypes = {
+    handleChild: PropTypes.func,
+  };
+  const baseURL = 'http://localhost:3000';
+  const [title, setTitle] = useState('');
+  const [location, setLocation] = useState('');
+  const [start, setStart] = useState('');
+  const [end, setEnd] = useState('');
+  const [days, setDays] = useState(new Array(5).fill(false));
+  const submitClass = () => {
+    const addNewClass = async () => {
+      for (let i = 0; i <= 5; i += 1) {
+        if (days[i]) {
+          try {
+            axios.get(`${baseURL}/classes/${i}`).then((prev) => {
+              const prevObj = prev.data;
+              prevObj.classes.push({
+                name: title,
+                location,
+                start,
+                end,
+              });
+              axios.put(`${baseURL}/classes/${i}`, prevObj);
+            });
+          } catch (error) {
+            throw new Error(error);
+          }
         }
-        addNewClass()
-
+      }
+    };
+    addNewClass();
+  };
+  const handleDay = (e, i) => {
+    const newDays = days;
+    newDays[i] = (e.target.checked);
+    setDays(newDays);
+  };
+  const handleTime = (e, i) => {
+    const splitTime = e.target.value.split(':');
+    const newTime = Number(splitTime[0]) * 60 + Number(splitTime[1]);
+    if (i === 0) {
+      setStart(newTime);
+    } else {
+      setEnd(newTime);
     }
-    const handleDay = (e, i) => {
-        var newDays = days
-        newDays[i] = (e.target.checked)
-        setDays(newDays)
-    }
-    const handleTime = (e, i) => {
-        const splitTime = e.target.value.split(":")
-        const newTime = Number(splitTime[0]) * 60 + Number(splitTime[1])
-        if (i===0){
-            setStart(newTime)
-        } else {
-            setEnd(newTime)
-        }
-    }
-    return (
-        <>
+  };
+  return (
+    <div className="flex justify-center align-middle items-center absolute top-0 w-screen h-screen z-10 m-0 p-0">
+      <form>
+        <button type="button" aria-label="close" id="close" className="w-screen h-screen absolute bg-white top-0 opacity-50 m-0 p-0" onClick={handleChild} />
+      </form>
 
-            <div className='flex justify-center align-middle items-center absolute top-0 w-screen h-screen z-10 m-0 p-0'>
-                <div className='w-screen h-screen absolute bg-white top-0 opacity-50 m-0 p-0' onClick={props.handleChild} />
-                <div className='absolute bg-slate-500 p-8 flex flex-col justify-center items-center '>
-                    <form  className='m-8 flex justify-between flex-col'>
-                        <input placeholder='Course Title' value={title} className='' onChange={(e) => setTitle(e.target.value)}/>
-                        <input placeholder='Course Location' className='m-4' onChange={(e) => setLocation(e.target.value)}/>
-                        <input type={"time"} className='m-8' onChange={(e) => handleTime(e, 0)}/>
-                        <input type={"time"} className='m-8' onChange={(e) => handleTime(e, 1)}/>
+      <div className="absolute bg-slate-500 p-8 flex flex-col justify-center items-center ">
+        <form className="m-8 flex justify-between flex-col">
+          <input placeholder="Course Title" value={title} className="" onChange={(e) => setTitle(e.target.value)} />
+          <input placeholder="Course Location" className="m-4" onChange={(e) => setLocation(e.target.value)} />
+          <input type="time" className="m-8" onChange={(e) => handleTime(e, 0)} />
+          <input type="time" className="m-8" onChange={(e) => handleTime(e, 1)} />
 
-                        <div className='flex items-center justify-center mb-8'>
-                            <input type={"checkbox"} name="m" onChange={(e) => handleDay(e, 0)}/>
-                            <label htmlFor="m" >M</label>
-                            <input type={"checkbox"} name="t" onChange={(e) => handleDay(e, 1)}/>
-                            <label htmlFor="t">T</label>
-                            <input type={"checkbox"} name="w" onChange={(e) => handleDay(e, 2)}/>
-                            <label htmlFor="w">W</label>
-                            <input type={"checkbox"} name="th" onChange={(e) => handleDay(e, 4)}/>
-                            <label htmlFor="th">T</label>
-                            <input type={"checkbox"} name="f" onChange={(e) => handleDay(e, 5)}/>
-                            <label htmlFor="f">F</label>
-                        </div>
-                        
-                    </form>
-                    <button className='bg-white' onClick={(e) => submitClass(e)}>Add Class</button>
+          <div className="flex items-center justify-center mb-8">
 
-                </div>
-            </div >
-        </>
-    )
+            <label htmlFor="m">
+              <input type="checkbox" id="m" onChange={(e) => handleDay(e, 0)} />
+              M
+            </label>
+
+            <label htmlFor="t">
+              <input type="checkbox" id="t" onChange={(e) => handleDay(e, 1)} />
+              T
+            </label>
+
+            <label htmlFor="w">
+              <input type="checkbox" id="w" onChange={(e) => handleDay(e, 2)} />
+              W
+            </label>
+
+            <label htmlFor="th">
+              <input type="checkbox" id="th" onChange={(e) => handleDay(e, 4)} />
+              T
+            </label>
+
+            <label htmlFor="f">
+              <input type="checkbox" id="f" onChange={(e) => handleDay(e, 5)} />
+              F
+            </label>
+          </div>
+
+        </form>
+        <button type="button" className="bg-white" onClick={(e) => submitClass(e)}>Add Class</button>
+
+      </div>
+    </div>
+  );
 }
 
-export default AddClass
+export default AddClass;
--- src/Views/ScheduleView.jsx
+++ src/Views/ScheduleView.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import Schedule from '../Components/Schedule'
-import AddClass from '../Components/AddClass'
+import React from 'react';
+import Schedule from '../Components/Schedule';
 
 function ScheduleView() {
   return (
     <div>
-        <Schedule/>
-        
+      <Schedule />
     </div>
-  )
+  );
 }
 
-export default ScheduleView
+export default ScheduleView;
