d9ee270d400c9fd868019089ac99ce82cbc5b308
jesssling <56201226+jesssling@users.noreply.github.com>
2023-03-23 02:04:26 -0400

    Updated userCard test files
    57.5% statements, 100% branch, 55.55 funcs, 55.26% lines

--- src/tests/userCardUI.test.js
+++ src/tests/userCardUI.test.js
@@ -6,19 +6,27 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 // import testing library functions
 import { render, screen } from '@testing-library/react';
-import { act } from 'react-test-renderer';
+import renderer, { act } from 'react-test-renderer';
 import UserCard from '../Components/UserCard';
+import getUserHistory from '../api/userProfileAPI';
 
 test('rendersAllStatBoxes', async () => {
-    const {getByText} = await act( async () => render(<UserCard userId="MockUsername"/>));
+    const {getByText} = await act( async () => render(<UserCard userId="validId"/>));
     expect(screen.getByText(/Total Classes Skipped/)).toBeInTheDocument();
     expect(screen.getByText(/Percent Classes Skipped/)).toBeInTheDocument();
     expect(screen.getByText(/Class Most Often Skipped/)).toBeInTheDocument();
     expect(screen.getByText(/Class Least Often Skipped/)).toBeInTheDocument();
 });
 
-test('rendersCorrectAvatar', async () => {
-    const { getByTestId } = await act(async () => render(<UserCard userId="MockUsername" />));
+test('rendersAvatar', async () => {
+    const { getByTestId } = await act(async () => render(<UserCard userId="validId" />));
     const avatar = getByTestId('PersonIcon');
     expect(avatar).toBeInTheDocument();
+});
+
+test('snapshot test', () => {
+    const component = renderer.create(<UserCard userId="validId"/>);
+    const domTreeJSON = component.toJSON();
+
+    expect(domTreeJSON).toMatchSnapshot();
 });
