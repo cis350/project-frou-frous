ba1e53ac341d4f721658e3c244be12e5ae4a5bd5
Erik Wei <erikwei@sas.upenn.edu>
2023-03-23 18:31:49 -0400

    fixed register test cases

--- src/Tests/RegisterTest2.test.js
+++ src/Tests/RegisterTest2.test.js
@@ -1,5 +1,3 @@
-
-
 /**
 * @jest-environment jsdom
 */
@@ -7,173 +5,159 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 // import testing library functions
-import { render, screen, fireEvent} from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { act } from 'react-test-renderer';
-import renderer from 'react-test-renderer';
-import Register  from "../Components/Register";
-import userEvent from '@testing-library/user-event';
-import {ToastContainer,toast} from "react-toastify";
+import { ToastContainer } from 'react-toastify';
+import Register from '../Components/Register';
 
-export const renderWithToastify = (component) => {
-  return (
-    render(
-      <div>
-        <ToastContainer/>
-        {component}
-      </div>
-    )
-  );
-};
+export const renderWithToastify = (component) => ( //eslint-disable-line
+  render(
+    <div>
+      <ToastContainer />
+      {component}
+    </div>,
+  )
+);
 
-test('check submit button', ()=> {
-    const { getByTestId } = render(<Register />);
-    const button = getByTestId("submitButton");
-    expect(button).toBeTruthy()
+test('check submit button', () => {
+  const { getByTestId } = render(<Register />);
+  const button = getByTestId('submitButton');
+  expect(button).toBeTruthy();
 });
 
-test('check home button', ()=> {
-    const { getByTestId } = render(<Register />);
-    const button = getByTestId("login");
-    expect(button).toBeTruthy()
+test('check home button', () => {
+  const { getByTestId } = render(<Register />);
+  const button = getByTestId('login');
+  expect(button).toBeTruthy();
 });
 
-
-  test("emailInputCheck", () => {
-    const { getByPlaceholderText} = render(<Register />);
-    const input = getByPlaceholderText("email");
-    expect(input).toBeInTheDocument();
-    fireEvent.change(input, {
-        target: { value: "gmail" }
-      });
-    expect(input.value).toBe("gmail");
+test('emailInputCheck', () => {
+  const { getByPlaceholderText } = render(<Register />);
+  const input = getByPlaceholderText('email');
+  expect(input).toBeInTheDocument();
+  fireEvent.change(input, {
+    target: { value: 'gmail' },
   });
+  expect(input.value).toBe('gmail');
+});
 
-  test("firstNameInputCheck", () => {
-    const { getByPlaceholderText} = render(<Register />);
-    const input = getByPlaceholderText("John");
-    expect(input).toBeInTheDocument();
-    fireEvent.change(input, {
-        target: { value: "Jane" }
-      });
-    expect(input.value).toBe("Jane");
+test('firstNameInputCheck', () => {
+  const { getByPlaceholderText } = render(<Register />);
+  const input = getByPlaceholderText('John');
+  expect(input).toBeInTheDocument();
+  fireEvent.change(input, {
+    target: { value: 'Jane' },
   });
-  test("lastNameInputCheck", () => {
-    const { getByPlaceholderText} = render(<Register />);
-    const input = getByPlaceholderText("Doe");
-    expect(input).toBeInTheDocument();
-    fireEvent.change(input, {
-        target: { value: "Prey" }
-      });
-    expect(input.value).toBe("Prey");
+  expect(input.value).toBe('Jane');
+});
+test('lastNameInputCheck', () => {
+  const { getByPlaceholderText } = render(<Register />);
+  const input = getByPlaceholderText('Doe');
+  expect(input).toBeInTheDocument();
+  fireEvent.change(input, {
+    target: { value: 'Prey' },
   });
-  test("usernameInputCheck", () => {
-    const { getByPlaceholderText} = render(<Register />);
-    const input = getByPlaceholderText("username");
-    expect(input).toBeInTheDocument();
-    fireEvent.change(input, {
-        target: { value: "poppy" }
-      });
-    expect(input.value).toBe("poppy");
+  expect(input.value).toBe('Prey');
+});
+test('usernameInputCheck', () => {
+  const { getByPlaceholderText } = render(<Register />);
+  const input = getByPlaceholderText('username');
+  expect(input).toBeInTheDocument();
+  fireEvent.change(input, {
+    target: { value: 'poppy' },
   });
+  expect(input.value).toBe('poppy');
+});
 
-  test("passwordInputCheck", () => {
-    const { getByPlaceholderText} = render(<Register />);
-    const input = getByPlaceholderText("password");
-    expect(input).toBeInTheDocument();
-    fireEvent.change(input, {
-        target: { value: "seed" }
-      });
-    expect(input.value).toBe("seed");
+test('passwordInputCheck', () => {
+  const { getByPlaceholderText } = render(<Register />);
+  const input = getByPlaceholderText('password');
+  expect(input).toBeInTheDocument();
+  fireEvent.change(input, {
+    target: { value: 'seed' },
   });
-
-  
-
-
+  expect(input.value).toBe('seed');
+});
 
 test('firstName textbox present Register', async () => {
-    const { getByPlaceholderText } = await act( async () => render(<Register />));
-    
-    const inputBox = getByPlaceholderText('John');
+  const { getByPlaceholderText } = await act(async () => render(<Register />));
 
-    expect(inputBox).toBeInTheDocument();
+  const inputBox = getByPlaceholderText('John');
+
+  expect(inputBox).toBeInTheDocument();
 });
 test('lastName textbox present Register', async () => {
-    const { getByPlaceholderText } = await act( async () => render(<Register />));
-    
-    const inputBox = getByPlaceholderText('Doe');
+  const { getByPlaceholderText } = await act(async () => render(<Register />));
 
-    expect(inputBox).toBeInTheDocument();
+  const inputBox = getByPlaceholderText('Doe');
+
+  expect(inputBox).toBeInTheDocument();
 });
 test('username textbox present Register', async () => {
-    const { getByPlaceholderText } = await act( async () => render(<Register />));
-    
-    const inputBox = getByPlaceholderText('username');
+  const { getByPlaceholderText } = await act(async () => render(<Register />));
 
-    expect(inputBox).toBeInTheDocument();
+  const inputBox = getByPlaceholderText('username');
+
+  expect(inputBox).toBeInTheDocument();
 });
 test('password textbox present Register', async () => {
-    const { getByPlaceholderText } = await act( async () => render(<Register />));
-    
-    const inputBox = getByPlaceholderText('password');
+  const { getByPlaceholderText } = await act(async () => render(<Register />));
 
-    expect(inputBox).toBeInTheDocument();
+  const inputBox = getByPlaceholderText('password');
+
+  expect(inputBox).toBeInTheDocument();
 });
 test('email textbox present Register', async () => {
-    const { getByPlaceholderText } = await act( async () => render(<Register />));
-    
-    const inputBox = getByPlaceholderText('email');
+  const { getByPlaceholderText } = await act(async () => render(<Register />));
 
-    expect(inputBox).toBeInTheDocument();
+  const inputBox = getByPlaceholderText('email');
+
+  expect(inputBox).toBeInTheDocument();
 });
 
-test("registerInvalidUsername", async() => {
-  const { getByPlaceholderText, getByTestId, getByText} = render(<Register />);
-  const input = getByPlaceholderText("username");
+test('registerInvalidUsername', async () => {
+  const { getByPlaceholderText, getByTestId } = render(<Register />);
+  const input = getByPlaceholderText('username');
   expect(input).toBeInTheDocument();
   fireEvent.change(input, {
-      target: { value: "" }
-    });
-  const submitButton = getByTestId("submitButton");
-  expect(submitButton).toBeInTheDocument();
-  fireEvent.click(submitButton);  
-});
-
-test("registerInvalidEmail", async() => {
-  const { getByPlaceholderText, getByTestId, getByText} = render(<Register />);
-  const input = getByPlaceholderText("email");
-  expect(input).toBeInTheDocument();
-  fireEvent.change(input, {
-      target: { value: "" }
-    });
-  const submitButton = getByTestId("submitButton");
+    target: { value: '' },
+  });
+  const submitButton = getByTestId('submitButton');
   expect(submitButton).toBeInTheDocument();
   fireEvent.click(submitButton);
-  
 });
-test("registerInvalidfirstName", async() => {
-  const { getByPlaceholderText, getByTestId, getByText} = render(<Register />);
-  const input = getByPlaceholderText("John");
+
+test('registerInvalidEmail', async () => {
+  const { getByPlaceholderText, getByTestId } = render(<Register />);
+  const input = getByPlaceholderText('email');
   expect(input).toBeInTheDocument();
   fireEvent.change(input, {
-      target: { value: "" }
-    });
-  const submitButton = getByTestId("submitButton");
+    target: { value: '' },
+  });
+  const submitButton = getByTestId('submitButton');
   expect(submitButton).toBeInTheDocument();
   fireEvent.click(submitButton);
-  
 });
-
-test("registerInvalidlastName", async() => {
-  const { getByPlaceholderText, getByTestId, getByText} = render(<Register />);
-  const input = getByPlaceholderText("Doe");
+test('registerInvalidfirstName', async () => {
+  const { getByPlaceholderText, getByTestId } = render(<Register />);
+  const input = getByPlaceholderText('John');
   expect(input).toBeInTheDocument();
   fireEvent.change(input, {
-      target: { value: "" }
-    });
-  const submitButton = getByTestId("submitButton");
+    target: { value: '' },
+  });
+  const submitButton = getByTestId('submitButton');
   expect(submitButton).toBeInTheDocument();
   fireEvent.click(submitButton);
-  
 });
 
-
+test('registerInvalidlastName', async () => {
+  const { getByPlaceholderText, getByTestId } = render(<Register />);
+  const input = getByPlaceholderText('Doe');
+  expect(input).toBeInTheDocument();
+  fireEvent.change(input, {
+    target: { value: '' },
+  });
+  const submitButton = getByTestId('submitButton');
+  expect(submitButton).toBeInTheDocument();
+  fireEvent.click(submitButton);
+});
