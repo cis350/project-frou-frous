4ac14976a0cdb85d2bebc3152d5554cf38559903
KLaneTX <klane@twistedx.com>
2023-04-01 16:47:17 -0400

    Added search bar to user page

--- src/Views/UserPageView.jsx
+++ src/Views/UserPageView.jsx
@@ -9,26 +9,36 @@ function UserPageView() {
   const [page, setPage] = useState('Profile'); // Either Profile or Timeline
 
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{ width: '100vw', minHeight: '100vh', backgroundColor: '#0D1B1E' }}
-    >
-      <Grid item xs={6} justifyContent="center">
-        {page === 'Timeline' && <Timeline userId={12345} page={page} />}
-        {page === 'Profile' && <UserCard userId={12345} />}
-      </Grid>
-      <Grid item xs={6} justifyContent="center">
-        <NavBar setPage={setPage} />
-        <center>
-          <div style={{ border: '3px solid #E5E5E5', maxHeight: '70%', borderRadius: '10px', padding: '3px', marginRight: '3px' }}>
-            <a href="/schedule/1234">
-              <ScheduleUser />
-            </a>
+    <div style={{ backgroundColor: '#0D1B1E' }}>
+      <center>
+        <div id="searchBar" className="input-group rounded-8" style={{ maxHeight: '50px' }}>
+          <div id="searchIcon">
+            <img alt="search" src="https://th.bing.com/th/id/R.602ef64bc31a62f9ebd523d97fc9f369?rik=JAPpFrIn7Yon0Q&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_194915.png&ehk=PY%2fOHmWzAr5CQnqxsnvx5nnpZAeNl7OJ5%2fHDzvIuHTo%3d&risl=&pid=ImgRaw&r=0" />
           </div>
-        </center>
+          <input id="chatSearch" type="text" className="border-0" style={{ textAlign: 'right' }} placeholder="User Search" />
+        </div>
+      </center>
+      <Grid
+        container
+        spacing={2}
+        sx={{ width: '100vw', minHeight: '100vh', backgroundColor: '#0D1B1E' }}
+      >
+        <Grid item xs={6} justifyContent="center">
+          {page === 'Timeline' && <Timeline userId={12345} page={page} />}
+          {page === 'Profile' && <UserCard userId={12345} />}
+        </Grid>
+        <Grid item xs={6} justifyContent="center">
+          <NavBar setPage={setPage} />
+          <center>
+            <div style={{ border: '3px solid #E5E5E5', maxHeight: '70%', borderRadius: '10px', padding: '3px', marginRight: '3px' }}>
+              <a href="/schedule/1234">
+                <ScheduleUser />
+              </a>
+            </div>
+          </center>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 }
 
