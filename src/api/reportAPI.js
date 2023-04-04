faf6c155c5f1dd62215c18f8bd6b80c30e84ef42
jesssling <victoria.fethke@yahoo.de>
2023-04-03 23:04:33 -0400

    added comments and updated api

--- src/api/reportAPI.js
+++ src/api/reportAPI.js
@@ -36,38 +36,3 @@ export async function sendComment(reportId, newComment) {
     return error;
   }
 }
-
-// export async function sendComment(reportId, newComment) {
-//   let result = await fetch(`http://localhost:8000/report/${reportId}`, {
-//     method: 'GET',
-//     headers: { 'content-type': 'application/json' },
-//   }).then((res) => {
-//     res.json();
-//     // return res;
-//   });
-//   result = result.comments.push({ newComment });
-//   fetch(`http://localhost:8000/report/${reportId}`, {
-//     method: 'PUT',
-//     headers: { 'content-type': 'application/json' },
-//     body: JSON.stringify(result),
-//   })
-//     .then((res) => res.json())
-//   // return fetch(`http://localhost:8000/report/${reportId}`, {
-//   //   method: 'PUT',
-//   //   headers: { 'content-type': 'application/json' },
-//   //   body: JSON.stringify(result),
-//   // }).then((res) => res.json())
-//     .catch((err) => {
-//       callback({ error: err });
-//     });
-// }
-
-// comments, {
-//   method: 'POST',
-//   headers: { 'content-type': 'application/json' },
-//   body: JSON.stringify(newComment),
-// }).then((res) => {
-//   callback(res);
-// }).catch((err) => {
-//   callback({ error: err });
-// });
