8c1417cbeb018f43b3c0e456d58a13240c47f16f
lanekk1 <lanekk@seas.upenn.edu>
2023-04-05 18:50:38 -0400

    Timeline update (#138)
    * Updated Colors
    
    * Added search bar to user page
    
    * id not finding for like feature
    
    * added like to add 1 but cannot take away
    
    * search bar good
    
    ---------
    
    Co-authored-by: KLaneTX <klane@twistedx.com>
    Co-authored-by: Ria Kulkarni <riakulkarni@lev-14081.apn.wlan.private.upenn.edu>

--- src/Components/Report.jsx
+++ src/Components/Report.jsx
@@ -4,18 +4,28 @@ import {
 } from '@mui/material';
 import PropTypes from 'prop-types';
 
-import { getReport, getPfp } from '../api/userPageAPI';
+import { getReport, getPfp, updateLikes } from '../api/userPageAPI';
 
 function Report(props) {
   const [name, setName] = useState('');
+  const [reportee, setReportee] = useState('');
   const [profilePhoto, setProfilePhoto] = useState('');
   const [photo, setPhoto] = useState('');
+  const [cap, setCaption] = useState('');
+  const [likeCount, setLikes] = useState([]);
+  const [like, setLike] = useState(100);
+  const [isLiked, setIsLiked] = useState(false);
   const { postId } = props;
+  const currentUser = sessionStorage.getItem('username');
 
   async function handleReport(reportResponse) {
-    const { reporterId, img } = reportResponse;
+    console.log('Res', reportResponse);
+    const { reporterId, img, reporteeId, caption, likes } = reportResponse;
     setName(reporterId);
     setPhoto(img);
+    setReportee(reporteeId);
+    setCaption(caption);
+    setLikes(likes);
 
     const profilePhotoResponse = await getPfp(); // Need to add this to swaggerHub API
     const { pfp } = profilePhotoResponse;
@@ -30,6 +40,31 @@ function Report(props) {
     }
   }
 
+  function validateUserLike(userName) {
+    if (likeCount.includes(userName)) {
+      setIsLiked(true);
+    }
+  }
+
+  function controlLike() {
+    validateUserLike(currentUser);
+    if (isLiked) {
+      likeCount.remove(currentUser);
+      setLike(like - 1);
+    } else {
+      likeCount.push(currentUser);
+      setLike(like + 1);
+    }
+    setLikes(likeCount);
+  }
+
+  const handleLike = (e) => {
+    e.preventDefault();
+    const newObj = { likeCount };
+    updateLikes(newObj, controlLike);
+    setIsLiked(!isLiked);
+  };
+
   useEffect(() => {
     fetchData();
   }, [postId]);
@@ -42,10 +77,29 @@ function Report(props) {
             <Avatar src={profilePhoto} alt={name} sx={{ width: 50, height: 50 }} />
           </Grid>
           <Grid item>
-            <div style={{ color: 'white', fontFamily: 'Open Sans, sans-serif' }}>{name}</div>
+            <div style={{ color: 'white', fontFamily: 'Open Sans, sans-serif' }}>
+              <b>
+                {name}
+              </b>
+              {' '}
+              reported
+              {' '}
+              <b>
+                {reportee}
+              </b>
+              {' '}
+              for skipping!
+            </div>
           </Grid>
         </Grid>
         <img src={photo} alt="" style={{ width: '100%', height: '100%', marginTop: 15, borderRadius: '10px' }} />
+        <center style={{ color: 'white' }}>
+          <p>
+            {' '}
+            {cap}
+            {' '}
+          </p>
+        </center>
       </CardContent>
       <CardActions>
         <Button id="comments" size="small" sx={{ backgroundColor: 'white', color: 'black' }}>
@@ -53,6 +107,10 @@ function Report(props) {
         </Button>
         <Button id="view" size="small" sx={{ backgroundColor: 'white', color: 'black' }}>
           View
+        </Button>
+        <Button className={`like-button ${isLiked && 'liked'}`} id="like" size="small" sx={{ backgroundColor: 'white', color: 'black' }} onClick={handleLike}>
+          <span className="likes-counter">{ `Like | ${like}` }</span>
+          Like
         </Button>
       </CardActions>
     </Card>
