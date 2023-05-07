01bc35cbd2651a80b63c5829ea8c96cf97b8bb2b
KLaneTX <klane@twistedx.com>
2023-05-07 09:59:10 -0400

    no pathing to index.html?

--- server.js
+++ server.js
@@ -289,7 +289,7 @@ webapp.put('/classes/:user/:day', async (req, resp) => {
 
 webapp.get('*',(req, resp) =>{
   console.log("GLOBAL GET DIRNAME:", __dirname);
-  resp.send(path.join(__dirname, 'index.html'));
+  resp.send('index.html');
 });
 
 module.exports = webapp;
