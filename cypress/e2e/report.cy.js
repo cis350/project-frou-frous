e9820f6e4ebff50473e66c55d276e60675b23a0c
marcel kida <47465692+marcelkida@users.noreply.github.com>
2023-05-08 15:25:43 -0700

    Login e2e testing (#172)
    * e2e testing for login done
    
    * changed ports back
    
    * e2e report
    
    ---------
    
    Co-authored-by: Ria Kulkarni <riakulkarni@Rias-MacBook-Pro-2.local>

---
+++ cypress/e2e/report.cy.js
@@ -0,0 +1,15 @@
+describe('report', () => {
+  it('should submit a valid report', () => {
+    cy.viewport(1280, 1200);
+    cy.visit('http://localhost:3000/');
+    cy.get('input[name="username"]').type('jess');
+    cy.get('input[name="password"]').type('jess');
+    cy.get('button[type="submit"]').click();
+    cy.get('a[href*="/report"]').click();
+    cy.get('input[placeholder="Reportee"]').type('Erik');
+    cy.get('textarea[placeholder="Caption"]').type('E2E Erik Skip');
+    cy.get('input[type=file]').selectFile('cypress/fixtures/cat.jpeg');
+    cy.get('button[name="post"]').click();
+    cy.get('textarea[placeholder="Caption"]').should('have.value', '');
+  });
+});
