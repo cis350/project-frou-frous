d201c2793763c1c38dfc6575cdc2b2ab1c669e3e
Erik Wei <erikwei@sas.upenn.edu>
2024-06-16 01:07:55 -0700

    NODE_ENV

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,1 +1,3 @@
-export const rootURL = process.env.ROOT_URL || 'http://localhost:5000';
+const production  = 'https://froufrous-72709a676ac7.herokuapp.com';
+const development = 'http://localhost:5000';
+export const rootURL = (process.env.NODE_ENV ? production : development);
